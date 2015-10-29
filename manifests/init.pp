bdf110d69634e8c2ef5e6e3c4c64bae65060c095
Adam Breaux <breauxaj@gmail.com>
2015-10-28 20:30:11 -0400

    match other modules

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,16 +1,44 @@
+# Class: wpcli
+#
+# This class downloads WP-CLI
+#
+# Parameters:
+#
+#  source: (default https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar )
+#    Location of the download
+#
+# Actions:
+#   - Install WP-CLI
+#   - Make wp-cli.phar executable
+#   - Symlink wp-cli.phar to wp-cli
+#
+# Requires:
+#
+# Sample Usage:
+#
+#  For a standard installation, use:
+#
+#    class { 'wpcli': }
+#
+#  To specify an alternate download link, use:
+#
+#    class { 'wpcli':
+#      source => 'http://alternate.domain.tld/get/phing-latest.phar'
+#    }
+#
 class wpcli (
   $source = 'https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar'
 ){
   exec { 'get-wpcli':
     path    => '/bin:/usr/bin',
     command => "wget ${source}'
-    cwd     => '/usr/bin',
-    creates => '/usr/bin/wp-cli.phar',
+    cwd     => '/usr/local/bin',
+    creates => '/usr/local/bin/wp-cli.phar',
     timeout => 10000,
-    onlyif  => "test ! -f /usr/bin/wp-cli.phar",
+    onlyif  => "test ! -f /usr/local/bin/wp-cli.phar",
   }
 
-  file { '/usr/bin/wp-cli.phar':
+  file { '/usr/local/bin/wp-cli.phar':
     ensure  => present,
     owner   => 'root',
     group   => 'root',
@@ -18,11 +46,11 @@ class wpcli (
     require => Exec['get-wpcli'],
   }
 
-  file { '/usr/bin/wp':
+  file { '/usr/local/bin/wp-cli':
     ensure  => 'link',
     owner   => 'root',
     group   => 'root',
-    target  => '/usr/bin/wp-cli.phar',
+    target  => '/usr/local/bin/wp-cli.phar',
     require => Exec['get-wpcli'],
   }
 
